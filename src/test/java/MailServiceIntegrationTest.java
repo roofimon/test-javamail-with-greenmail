cafe3aef74a2c59474dbbfa221152a8b5fa04848
roof <roof@odd-e.com>
2015-11-19 22:43:48 +0700

    Reset project

--- src/test/java/MailServiceIntegrationTest.java
+++ src/test/java/MailServiceIntegrationTest.java
@@ -10,16 +10,4 @@ import java.io.IOException;
 public class MailServiceIntegrationTest {
     private String[] RECIPIENT = {"roof@odd-e.co.th", "roofimon@gmail.com"};
 
-    @Test
-    public void testSendMailViaHotmailSMTP() throws MessagingException {
-        GenericSession session = new HotmailSession().invoke();
-        MailService mailService = new MailService(session, new TransactionConfirmationEmail());
-        mailService.send(RECIPIENT);
-    }
-    @Test
-    public void testSendMailViaGmailSMTP() throws IOException, MessagingException {
-        GenericSession session = new GmailSession().invoke();
-        MailService mailService = new MailService(session, new TransactionConfirmationEmail());
-        mailService.send(RECIPIENT);
-    }
 }
