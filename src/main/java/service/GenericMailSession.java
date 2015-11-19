448bc4a2fb86248899aa2a603bdc4c66bc0d7cae
roofimon <roofimon@gmail.com>
2015-11-19 10:33:51 +0700

    Refactor

--- src/main/java/service/GenericMailSession.java
+++ src/main/java/service/GenericMailSession.java
@@ -1,8 +1,11 @@
 package service;
 
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
@@ -23,14 +26,23 @@ public class GenericMailSession {
         props = System.getProperties();
     }
 
+    public MimeMessage createMimeMessage(String to, String subject, String body) throws MessagingException {
+        MimeMessage message = new MimeMessage(getSession());
+        message.setFrom(new InternetAddress(from));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to)) ;
+        message.setSubject(subject);
+        message.setText(body);
+        return message;
+    }
+
     public void send(MimeMessage message) throws MessagingException {
-        Transport transport = session.getTransport("smtp");
+        Transport transport = getSession().getTransport("smtp");
         transport.connect(host, port, from, pass);
         transport.sendMessage(message, message.getAllRecipients());
         transport.close();
     }
 
-    public Session getSession(){
+    private Session getSession(){
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", host);
         props.put("mail.smtp.user", from);
--- src/main/java/service/LegacyEmailService.java
+++ src/main/java/service/LegacyEmailService.java
@@ -5,31 +5,20 @@ import javax.mail.internet.*;
  * Created by roof on 11/18/15.
  */
 public class LegacyEmailService {
-    private static String USER_NAME = "massive.mail3r@gmail.com";  // GMail user name (just the part before "@gmail.com")
-    private static String PASSWORD = "N0mif00rA"; // GMail password
-    private static String RECIPIENT = "roofimon@gmail.com";
     private GenericMailSession session;
-    String from = USER_NAME;
-    String pass = PASSWORD;
 
     public void setSession(GenericMailSession session) {
         this.session = session;
     }
 
-    public void send(String[] args) {
-        String[] to = { RECIPIENT }; // list of recipient email addresses
+    public void send(String[] to) {
         String subject = "Java send mail example";
         String body = "Your transaction is completed !!!";
         try {
-            MimeMessage message = new MimeMessage(session.getSession());
-            message.setFrom(new InternetAddress(from));
-            InternetAddress[] toAddress = new InternetAddress[to.length];
-            getInternetAddress(to, toAddress);
-            addRecipient(message, toAddress);
-            message.setSubject(subject);
-            message.setText(body);
-
-            session.send(message);
+            for( int i = 0; i < to.length; i++ ) {
+                MimeMessage message = session.createMimeMessage(to[i], subject, body);
+                session.send(message);
+            }
         }
         catch (AddressException ae) {
             ae.printStackTrace();
@@ -39,19 +28,4 @@ public class LegacyEmailService {
         }
     }
 
-
-
-
-    private void addRecipient(MimeMessage message, InternetAddress[] toAddress) throws MessagingException {
-        for( int i = 0; i < toAddress.length; i++) {
-            message.addRecipient(Message.RecipientType.TO, toAddress[i]);
-        }
-    }
-
-    private void getInternetAddress(String[] to, InternetAddress[] toAddress) throws AddressException {
-        // To get the array of addresses
-        for( int i = 0; i < to.length; i++ ) {
-            toAddress[i] = new InternetAddress(to[i]);
-        }
-    }
 }
