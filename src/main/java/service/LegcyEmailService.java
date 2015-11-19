591efd5f00544e24a864eb940f4969e073523465
roof <roof@odd-e.com>
2015-11-19 22:49:49 +0700

    Extract GmailSession class

--- src/main/java/service/LegcyEmailService.java
+++ src/main/java/service/LegcyEmailService.java
@@ -17,37 +17,19 @@ public class LegcyEmailService {
         String subject = "Java send mail example";
         String body = "Welcome to JavaMail!";
 
-        sendFromGMail(from, pass, to, subject, body);
-    }
-
-    private static void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
-        Properties props = System.getProperties();
-        String host = "smtp.gmail.com";
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.user", from);
-        props.put("mail.smtp.password", pass);
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
-
-        Session session = Session.getDefaultInstance(props);
-        MimeMessage message = new MimeMessage(session);
+        GmailSession gmailSession = new GmailSession(from, pass).invoke();
+        Session session = gmailSession.getSession();
+        String host = gmailSession.getHost();
 
         try {
+            MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(from));
             InternetAddress[] toAddress = new InternetAddress[to.length];
-
-            // To get the array of addresses
-            for( int i = 0; i < to.length; i++ ) {
-                toAddress[i] = new InternetAddress(to[i]);
-            }
-
-            for( int i = 0; i < toAddress.length; i++) {
-                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
-            }
-
+            getRecipientInternetAddress(to, toAddress);
+            addRecipientToMessage(message, toAddress);
             message.setSubject(subject);
             message.setText(body);
+
             Transport transport = session.getTransport("smtp");
             transport.connect(host, from, pass);
             transport.sendMessage(message, message.getAllRecipients());
@@ -58,6 +40,52 @@ public class LegcyEmailService {
         }
         catch (MessagingException me) {
             me.printStackTrace();
+        }
+    }
+
+    private static void addRecipientToMessage(MimeMessage message, InternetAddress[] toAddress) throws MessagingException {
+        for( int i = 0; i < toAddress.length; i++) {
+            message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+        }
+    }
+
+    private static void getRecipientInternetAddress(String[] to, InternetAddress[] toAddress) throws AddressException {
+        // To get the array of addresses
+        for( int i = 0; i < to.length; i++ ) {
+            toAddress[i] = new InternetAddress(to[i]);
+        }
+    }
+
+    private class GmailSession {
+        private String from;
+        private String pass;
+        private String host;
+        private Session session;
+
+        public GmailSession(String from, String pass) {
+            this.from = from;
+            this.pass = pass;
+        }
+
+        public String getHost() {
+            return host;
+        }
+
+        public Session getSession() {
+            return session;
+        }
+
+        public GmailSession invoke() {
+            Properties props = System.getProperties();
+            host = "smtp.gmail.com";
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", host);
+            props.put("mail.smtp.user", from);
+            props.put("mail.smtp.password", pass);
+            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.auth", "true");
+            session = Session.getDefaultInstance(props);
+            return this;
         }
     }
 }
