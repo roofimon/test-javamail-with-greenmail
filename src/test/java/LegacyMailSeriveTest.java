e767f025ef497c2bcbf20bb9ecbf015ccfd46073
roof <roof@odd-e.com>
2015-11-23 14:19:48 +0700

    Remove MailService and Add LegacySeviceTest

---
+++ src/test/java/LegacyMailSeriveTest.java
@@ -0,0 +1,47 @@
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetup;
+import org.junit.Ignore;
+import org.junit.Test;
+import service.LegcyEmailService;
+import service.MailSession;
+import service.SMTPAccount;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by roof on 11/23/15.
+ */
+public class LegacyMailSeriveTest {
+    private static final int SMTP_TEST_PORT = 3025;
+    public GreenMail greenMail;
+
+    private String[] RECIPIENT = {"roof@odd-e.co.th", "roofimon@gmail.com"};
+    @Ignore
+    @Test
+    public void sendEmailViaGoogleSMTP() {
+        SMTPAccount smtpAccount = new SMTPAccount("massive.mail3r@gmail.com", "******", "smtp.gmail.com", 587);
+        MailSession gmailSession = new MailSession(smtpAccount);
+        LegcyEmailService mailService = new LegcyEmailService();
+        mailService.setGmailSession(gmailSession);
+        mailService.send(RECIPIENT);
+    }
+    @Test
+    public void sendEmailViaGreenMailSMTP() {
+        //Arrange
+        greenMail = new GreenMail(new ServerSetup(SMTP_TEST_PORT, null, "smtp"));
+        greenMail.start();
+        SMTPAccount smtpAccount = new SMTPAccount("fake@greenmail.com", "*******", "localhost", 3025);
+        MailSession fakeSession = new MailSession(smtpAccount);
+        LegcyEmailService mailService = new LegcyEmailService();
+        mailService.setGmailSession(fakeSession);
+
+        //Act
+        mailService.send(RECIPIENT);
+
+        //Assert
+        assertEquals("Your transaction is completed !!!", GreenMailUtil.getBody(greenMail.getReceivedMessages()[0]));
+        greenMail.stop();
+    }
+
+}
